e59df1734cb1e0b05a33cb9610c46c086c9ba1c2
Anton Sadovnik <222anton02@gmail.com>
2024-12-11 19:50:05 +0100

    finished header basic layout, started Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@public/images/logo.svg";
+import Facebook from "@public/images/facebook.svg";
+import Twitter from "@public/images/twitter.svg";
+import Instagram from "@public/images/instagram.svg";
+
 export default function index() {
-  return <div>Footer</div>;
+  const socialMedia = [
+    { url: "https://www.facebook.com/", svg: Facebook },
+    { url: "https://x.com/", svg: Twitter },
+    { url: "https://www.instagram.com/", svg: Instagram },
+  ];
+  return (
+    <footer className="bg-black">
+      <div className="py-5">
+        <div>
+          <Link href="/">
+            <Image src={Logo} alt="" />
+          </Link>
+        </div>
+        <p>
+          CineSphere is your go-to platform for a world of entertainment,
+          offering a vast library of films, TV shows, and exclusive content.{" "}
+        </p>
+        <div>
+          {socialMedia.map(({ url, svg }) => (
+            <div key={url}>
+              <Link href={url}>
+                <Image src={svg} alt="" />
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 }
